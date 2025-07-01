f981d860c47e5ef0a6206f6989e7d8a13887cdcd
t.naumann <t.naumann@gge-germany.de>
2025-07-01 17:43:56 +0200

    .

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       #
       forAllSystems = nixpkgs.lib.genAttrs [
         "x86_64-linux"
-        #"aarch64-darwin"
+        "aarch64-darwin"
       ];
 
       # ========== Extend lib with lib.custom ==========
